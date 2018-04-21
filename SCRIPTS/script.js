a4c1b4a1d567ed37f675826166a85574792c6738
Canberra Roo <anaiya.badi@gmail.com>
2018-04-21 08:53:23 -0400

    I made a mad libs

--- SCRIPTS/script.js
+++ SCRIPTS/script.js
@@ -1,19 +1,5 @@
-/* $("#hide").click(function(){
-  $("p").hide(hide);
-})
 
-$("#show").click(function(){
-  $("p").show(show);
-})
 
-$("button").click(function(){
-  $("p").hide(1000);
-}
-
-$("button").click(function(){
-  $("p").show(1000);
-}
-*/
 // first thing you need to do is make two functions. the format is function functionName(){code}. One should be called Hide, one shoud be called Show.
 // After
 function hide() {
@@ -24,10 +10,64 @@ function show() {
   $("img").show();
 }
 
-$("#hide").click( () => {
-     hide()
+/*$("#hide").click( () => {
+     hide();
 });
 
 $("#show").click( () => {
      show()
 });
+*/
+
+
+$("button").click( () => {
+
+   let name = $("#input1").val();
+   $("p").append(`${name} puked `);
+
+
+
+});
+
+$("button").click( () => {
+
+   let name = $("#input2").val();
+   $("p").append(`${name}  `);
+
+
+});
+
+$("button").click( () => {
+
+   let name = $("#input3").val();
+   $("p").append(`${name} because `);
+
+
+
+});
+
+$("button").click( () => {
+
+   let name = $("#input4").val();
+   $("p").append(`${name}  `);
+
+
+});
+
+$("button").click( () => {
+
+   let name = $("#input5").val();
+   $("p").append(`${name} onto `);
+
+
+
+});
+
+$("button").click( () => {
+
+   let name = $("#input1").val();
+   $("p").append(`${name}  `);
+
+
+
+});
