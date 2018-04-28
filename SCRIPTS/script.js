61d207f044ea76e3c8b73d39685abe64ebb04379
Canberra Roo <anaiya.badi@gmail.com>
2018-04-27 20:03:39 -0400

    Whooo! donezo!

---
+++ SCRIPTS/script.js
@@ -0,0 +1,70 @@
+
+$("button").click( () => {
+
+   let name = $("#input1").val();
+   $("p").append(`Once there was a ${name} named `);
+
+
+
+});
+
+$("button").click( () => {
+
+   let name = $("#input2").val();
+   $("p").append(`${name} who lived in `);
+
+
+});
+
+$("button").click( () => {
+
+   let name = $("#input3").val();
+   $("p").append(`${name}.`);
+
+
+
+});
+
+$("button").click( () => {
+
+   let name = $("#input2").val();
+   $("p").append(`${name} had both a school enemy, `);
+
+
+});
+
+$("button").click( () => {
+
+   let name = $("#input4").val();
+   $("p").append(`${name} and a pure evil one `);
+
+
+
+});
+
+$("button").click( () => {
+
+   let name = $("#input5").val();
+   $("p").append(`${name}. `);
+
+
+
+});
+
+$("button").click( () => {
+
+let name = $("#input2").val();
+$("p").append(`${name} got sorted by the sorting  `);
+
+
+
+});
+
+$("button").click( () => {
+
+let name = $("#input6").val();
+$("p").append(`${name} into Gryffindor `);
+
+
+
+});
